92f101c38c58af6b97e1796eadbaf6c2a7aba6d9
labyrinth03 <leedong1003@naver.com>
2021-06-30 23:19:21 +0900

    added voicechannel

--- Prototype.py
+++ Prototype.py
@@ -1,23 +1,24 @@
 import discord
 from discord.ext import commands
 import nest_asyncio
+import time 
 nest_asyncio.apply()
  
 bot = commands.Bot(command_prefix='0_<')
-TOKEN = 'ODI1Mzg0NTMwODE4MjM2NDI3.YF9JPw.zaevLZt9IL1u2eo8oK3-MybZvOs'
+TOKEN = ''
 
 @bot.event
 async def on_ready():
-    print(bot.user.name, 'has woken up.')
+    print(bot.user.name, 'has woken up.')                       # To check bot run
     await bot.change_presence(status=discord.Status.online, activity=discord.Game('Greeting'))
     
 @bot.event
-async def on_message(message):
-    if message.author == bot.user:
+async def message_react(message):
+    if message.author == bot.user:                              # except bot's msg
         return
     
-    if message.content.startswith('@hello'):
-        cchannel = message.channel   #current channel
+    if message.content.startswith('@hello'):                    # hello msg
+        cchannel = message.channel                              # cchannel = current channel
         await cchannel.send('Hello World!')
         
         def check(m):
@@ -34,9 +35,39 @@ async def on_message(message):
         def check(reaction, user):
             return user == message.author and str(reaction.emoji) == ':thinking:'
 
-        try:
+        try: 
             reaction, user = await bot.wait_for('reaction_add', timeout=10.0, check=check)
+        except:
+            await cchannel.send(":thinking:")
+            
+    if message.content.startswith('@createstage'):              # Create Stage Channel for temporary 
+        cchannel = message.channel
+        msglist = message.content.split(" ")
+        stage = msglist[1]
+        await create_category(name = stage, *, overwrites=None, reason=None, position=None)
+        await create_stage_channel(name = stage, *, topic=None, category=stage, overwrites=None, reason=None, position=None)
+        await create_text_channel(name = "For_" + stage, *, overwrites=None, category=stage, reason=None, **options)
+        if Voicechannel.members = None:
+            await cchannel.send("No members detected. This category will be deleted in a day.")
+            time.sleep(86400)
+            await voice.delete()
+            await voice.delete()
+            await "For_" + stage.delete()
+    
+
+    if message.content.startswith('@createvoice'):              # Create Voice Channel with its temporary msg channel
+        cchannel = message.channel
+        msglist = message.content.split(" ")
+        voice = msglist[1]
+        await create_category(name = voice, *, overwrites=None, reason=None, position=None)
+        await create_stage_channel(name = voice, *, topic=None, category=voice, overwrites=None, reason=None, position=None)
+        await create_text_channel(name = "For_" + voice, *, overwrites=None, category=voice, reason=None, **options)
+        if Voicechannel.members = None:
+            await cchannel.send("No members detected. This category will be deleted in a day.")
+            time.sleep(86400)
+            await voice.delete()
+            await voice.delete()
+            await "For_" + voice.delete()
 
 
-
-bot.run('ODI1Mzg0NTMwODE4MjM2NDI3.YF9JPw.zaevLZt9IL1u2eo8oK3-MybZvOs')
+bot.run('')
